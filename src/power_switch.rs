8e32773aec143c8f25e026ffff2ea5beb242f054
Andrey Kruglov <kruglov.it@gmail.com>
2022-05-31 13:10:10 +0300

    home3

--- src/power_switch.rs
+++ src/power_switch.rs
@@ -1,14 +1,16 @@
 //! Module describes power switch device for smart house
 
+use std::fmt::{self, Display};
+
 /// Describes state of power switch
-#[derive(Debug)]
+#[derive(Debug, Display, Clone)]
 pub enum SwitchState {
     On,
     Off,
 }
 
 /// Describes smart power switch
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct PowerSwitch {
     state: SwitchState,
     description: String,
@@ -38,5 +40,15 @@ impl PowerSwitch {
     /// Returns current power consumption of the switch
     pub fn power_consumption(&self) -> f64 {
         self.power_consumption
+    }
+}
+
+impl Display for PowerSwitch {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Power Switch (state: {}, description: \"{}\", power consumption: {})",
+            self.state, self.description, self.power_consumption
+        )
     }
 }
--- src/thermometer.rs
+++ src/thermometer.rs
@@ -1,7 +1,8 @@
 //! Module describes thermometer device for smart house
 
+use std::fmt;
 /// Describes smart thermometer
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct Thermometer {
     temperature: f64,
 }
@@ -10,5 +11,11 @@ impl Thermometer {
     /// Returns current temperature of the thermometer
     pub fn temperature(&self) -> f64 {
         self.temperature
+    }
+}
+
+impl fmt::Display for Thermometer {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Thermometer (temperature: {})", self.temperature)
     }
 }
