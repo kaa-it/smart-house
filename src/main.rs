345a118f46c5b9f6d2d5dcfad92498b706a882a8
Andrey Kruglov <kruglov.it@gmail.com>
2022-06-25 15:54:34 +0300

    fix lifetime

--- src/main.rs
+++ src/main.rs
@@ -22,14 +22,14 @@ impl DeviceInfoProvider for OwningDeviceInfoProvider {
     }
 }
 
-struct BorrowingDeviceInfoProvider<'a, 'b, 'c, 'd> {
+struct BorrowingDeviceInfoProvider<'a> {
     switch1: &'a PowerSwitch,
-    switch2: &'b PowerSwitch,
-    thermometer1: &'c Thermometer,
-    thermometer2: &'d Thermometer,
+    switch2: &'a PowerSwitch,
+    thermometer1: &'a Thermometer,
+    thermometer2: &'a Thermometer,
 }
 
-impl<'a, 'b, 'c, 'd> DeviceInfoProvider for BorrowingDeviceInfoProvider<'a, 'b, 'c, 'd> {
+impl<'a> DeviceInfoProvider for BorrowingDeviceInfoProvider<'a> {
     fn report(&self, room_name: &str, device_name: &str) -> Option<String> {
         match (room_name, device_name) {
             ("Dinning room", "therm1") => Some(format!("{}", self.thermometer1)),
